4931987c2b90517bac15162aeabf045dfeae48aa
skrjsghd <alsrjsghd1@gmail.com>
2024-04-15 19:10:35 +0900

    CHORE : add cn util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function deepCompare<T extends Record<string, any>>(
   obj1: T,
   obj2: T,

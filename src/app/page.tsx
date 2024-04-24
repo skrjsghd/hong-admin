3ca07a9dd461c7f602e79a4c4818a976f4949dd8
skrjsghd <alsrjsghd1@gmail.com>
2024-04-24 15:47:56 +0900

    CHORE : presentation link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { ConnectionForm } from "@/components/connection-form";
+import Link from "next/link";
 
 export default async function Page() {
   return (
     <section className="h-svh">
+      <Link
+        href="https://docs.google.com/presentation/d/1fKkMOrrrQmYmlC-Ft2caGVw7qLFzV45A0tC67xUfj_c/edit?usp=sharing"
+        target="_blank"
+      >
+        발표자료로
+      </Link>
       <ConnectionForm />
     </section>
   );

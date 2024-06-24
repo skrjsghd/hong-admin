f53281cd30105937934778f25e1bab9b610470e7
skrjsghd <alsrjsghd1@gmail.com>
2024-06-24 12:49:56 +0900

    FIX : date parsing in RowField component

--- src/components/_table/row-field.tsx
+++ src/components/_table/row-field.tsx
@@ -28,8 +28,11 @@ function RowField({ columnInformation, value, onChangeValue }: RowFieldProps) {
       onChangeValue(v);
     },
   };
-  const parseDate = (date: Date | null) => {
+  const parseDate = (date: Date | string | null) => {
     if (!date) return "";
+    if (typeof date === "string") {
+      date = new Date(date);
+    }
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
